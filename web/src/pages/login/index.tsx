4b0df03b17cdfddf92b557392fa837dec5add968
Pedro Tosta <pedrotosta11@gmail.com>
2023-09-09 10:15:15 -0400

    Página Login e Completar Registro

---
+++ web/src/pages/login/index.tsx
@@ -0,0 +1,24 @@
+import {  } from 'react';
+
+import { Container, Header, BoxContent, Label, Input, Submit } from './styles';
+
+import LogoMobile from '.../assets/logo_large.svg';
+
+function Login(){
+    return(
+        <Container>
+            <Header>
+                <img className='LogoMobile' src={LogoMobile} alt=""/>
+            </Header>
+            <BoxContent>
+                <Label>E-mail</Label>
+                <Input></Input>
+                <Label>Senha</Label>
+                <Input></Input>
+                <Submit>Realizar Login</Submit>
+            </BoxContent>
+        </Container>
+    );
+}
+
+export default Login;
