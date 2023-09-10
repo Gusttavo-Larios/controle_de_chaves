353767403ebf71bd111651477ae653b79e460b20
Gusttavo <eg9653641@gmail.com>
2023-09-10 12:32:10 -0300

    refactor: Substituição de componentes especificos por componentes genéricos.

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,22 +1,36 @@
-import { Container, Header, BoxContent, Label, Input, Submit } from './styles';
+import { Label } from "app/components/Label";
+import { Container, Header } from "./styles";
 
-import LogoMobile from 'app/assets/logo_large.svg';
+import LogoMobile from "app/assets/logo_large.svg";
+import { Form } from "app/components/FormGroup";
+import { Input } from "app/components/Input";
+import { Button } from "app/components/Button";
 
-function Login(){
-    return(
-        <Container>
-            <Header>
-                <img className='LogoMobile' src={LogoMobile} alt=""/>
-            </Header>
-            <BoxContent>
-                <Label>E-mail</Label>
-                <Input></Input>
-                <Label>Senha</Label>
-                <Input></Input>
-                <Submit>Realizar Login</Submit>
-            </BoxContent>
-        </Container>
-    );
+function Login() {
+  return (
+    <Container>
+      <Header>
+        <img className="LogoMobile" src={LogoMobile} alt="" />
+      </Header>
+      <Form>
+        <Form.Groups>
+          <Form.Group>
+            <Label>E-mail</Label>
+            <Input type="email" placeholder="ex: nome.sobrenome@ifmt.edu.br" />
+          </Form.Group>
+          <Form.Group>
+            <Label>Senha</Label>
+            <Input type="password" placeholder="******" />
+          </Form.Group>
+        </Form.Groups>
+
+        {/* <Submit>Realizar Login</Submit> */}
+        <Button type="submit" style={{ marginTop: "4rem" }}>
+          Realizar Login
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default Login;
