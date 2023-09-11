0cbc9d31a99a9cc7f186ea3d4e8fe59e9b656b8e
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-09-11 19:35:15 -0400

    Setup Back-end

---
+++ back-end/src/main/java/br/edu/ifmt/chaveReservada/ChaveReservadaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.chaveReserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaveReservadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaveReservadaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/br/edu/ifmt/chaveReserva/ChaveReservadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.chaveReserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaveReservadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
