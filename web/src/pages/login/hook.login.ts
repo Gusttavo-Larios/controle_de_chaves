3860d871e34afc9b53ec3df746c08a404fd0df6d
dev <gustavolarios077@gmail.com>
2023-11-02 18:05:41 -0400

    Oganizado o arquivo hook.login

--- web/src/pages/login/hook.login.ts
+++ web/src/pages/login/hook.login.ts
@@ -1,9 +1,9 @@
+import { useContext } from "react";
 import { SubmitHandler } from "react-hook-form";
 
 import { TLoginForm } from "./form_validator.user";
 
 import { LoginUseCaseImpl } from "app/application/use_case_impl/use_case_impl.login";
-import { useContext } from "react";
 import { AlertContext } from "app/contexts/modal/context.modal";
 
 export function useLogin() {
@@ -12,11 +12,8 @@ export function useLogin() {
 
   const onSubmit: SubmitHandler<TLoginForm> = async (data) => {
     try {
-      console.log(data)
       await loginUseCaseImpl.authenticate(data);
     } catch (error: any) {
-      console.log(error)
-
       openAlert({
         is_dialog: false,
         message: error.message,
