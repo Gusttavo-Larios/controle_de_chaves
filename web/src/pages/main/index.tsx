867273937710dcbe339367089d6c400fa1dcb001
dev <gustavolarios077@gmail.com>
2023-11-04 11:37:23 -0400

    refactor: Remoção de código desnecessário do componente pages/Main.

--- web/src/pages/main/index.tsx
+++ web/src/pages/main/index.tsx
@@ -5,15 +5,18 @@ import { mainFormRules, TMainForm } from "./form_validator.main";
 
 import { useMain } from "./hook.main";
 
-import { Container, Header, Results, ResultBox, ResultLabel } from "./styles";
+import { Results, ResultBox, ResultLabel } from "./styles";
 
 import Logo from "app/assets/logo_small.svg";
 import Menu from "app/assets/menu.svg";
+
+import { Body } from "app/layout/internal/Body";
 
 import { Form } from "app/components/FormGroup";
 import { Button } from "app/components/Button";
 import { Label } from "app/components/Label";
 import { Input } from "app/components/Input";
+import { Badge } from 'app/components/Badge';
 
 function main() {
   const { keyList, onSubmit } = useMain();
@@ -28,12 +31,7 @@ function main() {
   });
 
   return (
-    <Container>
-      <Header>
-        <img className="Logo" src={Logo} alt="" />
-        <img className="Menu" src={Menu} alt="" />
-      </Header>
-
+    <Body>
       <Form
         style={{ width: "90%", marginTop: "3rem" }}
         onSubmit={handleSubmit(onSubmit)}
@@ -72,37 +70,19 @@ function main() {
           <ResultBox key={item.id}>
             <ResultLabel>
               <Label style={{ fontSize: "20px" }}>{item.roomName}</Label>
-              <Button
+              <Badge
                 type="button"
                 variant={
                   item.status === "Disponível" ? "BLUE_300" : "YELLOW_400"
                 }
               >
                 {item.status}
-              </Button>
+              </Badge>
             </ResultLabel>
           </ResultBox>
         ))}
-        {/* <ResultBox>
-                    <ResultLabel>
-                        <Label style={{ fontSize: '20px'}}>#SL001</Label>
-                        <Button type="button">Disponível</Button>
-                    </ResultLabel>
-                </ResultBox>
-                <ResultBox>
-                    <ResultLabel>
-                        <Label style={{ fontSize: '20px'}}>#SL002</Label>
-                        <Button type="button" variant="YELLOW_400">Indisponível</Button>
-                    </ResultLabel>
-                </ResultBox>
-                <ResultBox>
-                    <ResultLabel>
-                        <Label style={{ fontSize: '20px'}}>#SL003</Label>
-                        <Button type="button">Disponível</Button>
-                    </ResultLabel>
-                </ResultBox>                 */}
       </Results>
-    </Container>
+    </Body>
   );
 }
 
--- web/src/pages/main/styles.ts
+++ web/src/pages/main/styles.ts
@@ -1,24 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  padding-top: 5.6rem;
-  display: flex;
-  flex: 1;
-  align-items: center;
-  flex-direction: column;  
-`;
-
-export const Header = styled.header`
-    position: fixed;
-    top: 0;
-    margin-top: 1rem;
-    width: 90%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-`;
-
 export const Results = styled.div`
     width: 90%;
     margin-top: 3rem;    
