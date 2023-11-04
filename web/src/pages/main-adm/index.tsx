6b547c3fb0c7ffda1dfbbeb988f883aeb35d7ee1
dev <gustavolarios077@gmail.com>
2023-11-04 11:48:14 -0400

    refactor: Remoção do componente Text do componente referente a opções de navegação de administrador.

--- web/src/pages/main-adm/index.tsx
+++ web/src/pages/main-adm/index.tsx
@@ -2,12 +2,10 @@ import { useNavigate } from 'react-router-dom';
 
 import { Area, BaseStyledAdministrationOption } from './styles';
 
-
 import User from 'app/assets/user.svg';
 import Key from 'app/assets/key.svg';
 
 import { Body } from 'app/layout/internal/Body';
-import { Text } from 'app/components/Text';
 
 function mainAdm() {
     const navigate = useNavigate()
@@ -17,11 +15,11 @@ function mainAdm() {
             <Area>
                 <BaseStyledAdministrationOption type="button" onClick={() => navigate("/administrativo/servidores")} >
                     <img src={User} alt="Imagem do contorno de uma pessoa" />
-                    <Text type="TEXT_SMALL">Servidores</Text>
+                    Servidores
                 </BaseStyledAdministrationOption>
                 <BaseStyledAdministrationOption type="button" onClick={() => navigate("/administrativo/chaves")} >
                     <img src={Key} alt="Imagem do contorno de uma chave" />
-                    <Text type="TEXT_SMALL">Chaves</Text>
+                    Chaves
                 </BaseStyledAdministrationOption>
             </Area>
         </Body>
